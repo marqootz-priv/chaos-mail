4f3900efc19fbd072376d843d90efccdc0b4d1e4
Mark Manfrey <mark.manfrey@icloud.com>
2026-01-01 13:14:27 -0800

    Initial Commit

---
+++ chaos-ctrl-mail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chaos-ctrl-mail
+//
+//  Created by Mark Manfrey on 1/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chaos-ctrl-mail/chaos_ctrl_mailApp.swift
@@ -0,0 +1,17 @@
+//
+//  chaos_ctrl_mailApp.swift
+//  chaos-ctrl-mail
+//
+//  Created by Mark Manfrey on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct chaos_ctrl_mailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
